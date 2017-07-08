c758534a835016d9d90a0c4399925b0125b13494
Dylan Clelland <dylanclelland@gmail.com>
2017-07-08 15:49:28 -0700

    Fixed the naming of some of the Function, Variables, Objects etc.
    Fixed the naming of the Functions etc, to meet requirements.

--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -11,56 +11,56 @@ var green;
 var quoteList = [
     {
     quote: "Only I can change my life. No one can do it for me.",
-    author: "Carol Burnett",
+    source: "Carol Burnett",
     citation: "",
     year:  "",
     tags: 'Motivation'
     },
     {
     quote: "Well done is better than well said.",
-    author: "Benjamin Franklin",
+    source: "Benjamin Franklin",
     citation: "",
     year:  "",
     tags: 'CEO'
     },
     {
     quote: "Quality is not an act, it is a habit.",
-    author: "Aristole",
+    source: "Aristole",
     citation: "",
     year:  "",
     tags: 'Motivation'
   },
   {
     quote: "If you can dream it, you can do it.",
-    author: "Walt Disney",
+    source: "Walt Disney",
     citation: "",
     year:  "",
     tags: 'Motivational'
   },
   {
     quote: "Think of all the beauty still left around you and be happy.",
-    author: "Anne Frank",
+    source: "Anne Frank",
     citation: "",
     year:  "",
     tags: 'Life'
   },
   {
     quote: "Summer means happy times and good sunshine. It means going to the beach, going to Disneyland, having fun.",
-    author: "Brian Wilson",
+    source: "Brian Wilson",
     citation: "",
     year:  "",
     tags: 'Summer Time'
   },
   {
     quote: "The word 'happy' would lose its meaning if it were not balanced by sadness.",
-    author: "Carl Jung",
+    source: "Carl Jung",
     citation: "",
     year:  "",
     tags: 'Sad Meaning'
   },
   {
     quote: "I have been very blessed in my life and rewarded with good friends and good health. I am grateful and happy to be able to share this.",
-    author: "Eric Idle",
+    source: "Eric Idle",
     citation: "",
     year:  "",
     tags: 'Happy'
@@ -85,7 +85,7 @@ function print(quoteList) {
 
     message = '<p class="quote">' + quotes.quote + '</p>';
     message += '<p>' + quotes.tags + '</p>';
-    message += '<p class="source">' + quotes.author ;
+    message += '<p class="source">' + quotes.source ;
 
     if (quotes.citation){
       message += '<span class="citation">' + quotes.citation + '</span>';
